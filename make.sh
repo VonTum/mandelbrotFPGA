9cb6df1a0a2de55718bfdd80fa4e2c5d72cc70fd
Lennart Van Hirtum <lennart.vanhirtum@gmail.com>
2025-07-28 06:24:56 +0200

    Initial Mandelbrot stuff

---
+++ make.sh
@@ -0,0 +1,8 @@
+/home/lennart/Desktop/sus-compiler/target/release/sus_compiler\
+    mandelbrot.sus float.sus\
+    --debug print-flattened\
+    --debug print-instantiated-modules\
+    --debug-whitelist SlowIterator2\
+    --debug-whitelist SlowIteratorState\
+    --standalone MandelbrotStep\
+    --standalone-file mandelbrotFPGA.srcs/sources_1/new/sus_codegen.sv
